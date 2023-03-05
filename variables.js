02dfdbccefee4f4aa67a688256856db2fb3a14ac
Your NameCarlos <you@example.com>
2023-03-05 22:27:04 +0100

    Ejercicios de variables JS Carlos Muñoz

---
+++ variables.js
@@ -0,0 +1,42 @@
+// variable sin valor UNDEFINED
+let variableSinValor;
+console.log('Una variable sin valor devuelve ' + variableSinValor);
+
+// variables boolean
+let booleano1 = true;
+console.log('Esto es un booleano con valor ' + booleano1);
+
+let booleano2 = false;
+console.log('Esto es un booleano con valor ' + booleano2);
+
+// variable PI
+const PI = 3.14;
+console.log('El valor de PI es ' + PI);
+
+// variable TAU
+const TAU = PI * 2;
+console.log('El valor de TAU es ' + TAU);
+
+// varible string
+const miNombre = 'Carlos';
+console.log('Me llamo ' + miNombre);
+
+// varible number
+const miNumeroFav = 7;
+console.log('Mi número de la suerte es el ' + miNumeroFav);
+
+// variable boolean personal
+const isNew = true;
+console.log('Do you really want to update your system? ' + isNew + '.');
+
+// console.log contatenación
+console.log('Mi nombre es ' + miNombre + ' y mi número favorito es el ' + miNumeroFav + '.');
+
+// string por console
+const myString = 'Seré un crack en JavaScript al terminar el bootcamp';
+console.log(myString.toUpperCase());
+
+// variable modificar valor
+let cantPedida = 5;
+cantPedida = 3;
+console.log('Tu último pedido fué de ' + cantPedida + ' unds.');
